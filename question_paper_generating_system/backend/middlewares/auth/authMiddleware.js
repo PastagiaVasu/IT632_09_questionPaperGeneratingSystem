492d1d50330e1a18ff642af48cca28cdbb125550
Sagar Variya <77156364+Sagar-v4@users.noreply.github.com>
2022-04-14 15:21:55 +0530

    link bug fixed

--- question_paper_generating_system/backend/middlewares/auth/authMiddleware.js
+++ question_paper_generating_system/backend/middlewares/auth/authMiddleware.js
@@ -1,30 +1,30 @@
 const expressAsyncHandler = require("express-async-handler");
 
 const jwt = require("jsonwebtoken");
-const User = require("../../model/user/user");
+const Faculty = require("../../model/user/user");
 
-const authMiddleware = expressAsyncHandler(async (req,res,next)=> {
+const authMiddleware = expressAsyncHandler(async (req, res, next) => {
     let token;
 
-    if(req?.headers?.authorization?.startsWith('Bearer')){
+    if (req?.headers?.authorization?.startsWith("Bearer")) {
         try {
             token = req.headers.authorization.split(' ')[1];
-            if(token){
-                const decoded = jwt.verify(token,process.env.JWT_KEY);
+            if (token) {
+                const decoded = jwt.verify(token, process.env.JWT_KEY);
 
-                //find the user by id
-                const user = await User.findById(decoded?.id).select("-password");
+                //find the faculty by id
+                const faculty = await Faculty.findById(decoded?.id).select("-password");
 
-                //attach the user to the request object
-                req.user = user;
+                //attach the faculty to the request object
+                req.faculty = faculty;
                 next();
-            }
-            else{
-                throw new ("There is no token attached to header");
             }
         } catch (error) {
             throw new Error("Not authorized token expired, login again");
         }
+    }
+    else {
+        throw new Error("There is no token attached to header");
     }
 });
 
